ff918c0512921096a6789b9b25ff4e14fd181975
UZI <dyasananta6@gmail.com>
2025-05-31 15:33:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday, Jihan 💖/nOn this special day, I just want you to know how much you mean to me. Youre not just someone who came into my life, youre the reason behind my smile every single day. May all the beautiful things in life always find their way to you, because you truly deserve the best. Im grateful to have met you, and I hope in the years to come, I can keep celebrating moments like this. Happy birthday, Jihan. Keep shining, like a star that never fades in my night sky.').split('')
+    const titles = ('Happy Birthday, Jihan 💖\nOn this special day, I just want you to know how much you mean to me. Youre not just someone who came into my life, youre the reason behind my smile every single day. May all the beautiful things in life always find their way to you, because you truly deserve the best. Im grateful to have met you, and I hope in the years to come, I can keep celebrating moments like this. Happy birthday, Jihan. Keep shining, like a star that never fades in my night sky.').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -11,7 +11,7 @@ onload = () => {
       if (index < titles.length) {
         titleElement.innerHTML += titles[index];
         index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
+        setTimeout(appendTitle, 100); // 1000ms delay
       }
     }
 
